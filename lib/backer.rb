9a32c95d03433d4b98b2fe717fe3a7031a72038e
Ken Goad <goadke@g.cofc.edu>
2019-12-01 16:10:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,7 @@ class Backer
   
   def back_project(project)
     backed_projects << project
+    project_backers << self
   end
   
   # Once both classes have their attributes and readers set up, write a method on the Backer class called back_project() that takes in a Project instance and adds the project to its @backed_projects attribute.
